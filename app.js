810dfa447f299af2ff3b6bceb9fd789e8d397efe
Escobar Jonathan <jony.escobar14@gmail.com>
2023-06-13 10:09:10 -0300

    Edit

--- app.js
+++ app.js
@@ -1,23 +1,19 @@
 const 
-boton_left=document.querySelector(".boton-left"),
-boton_right=document.querySelector(".boton-right"),
+btn_izq=document.querySelector(".boton-left"),
+btn_der=document.querySelector(".boton-right"),
 slider=document.querySelector("#slider"),
 carrousel=document.querySelectorAll(".carrusel-section"),
-final=document.querySelector(".final")
-let op=0,
-tamaño=100 / carrousel.length,
-cont=0,
-contador2,
-contador3;
+final=document.querySelector(".final");
+let op=0, tamaño=100 / carrousel.length, cont=0, contador2, contador3;
 
-if (boton_right) {
-    boton_right.addEventListener("click",()=> Right());
+if (btn_der) {
+    btn_der.addEventListener("click",()=> derecha());
 }
-if (boton_left) {
-    boton_left.addEventListener("click",()=> left());
+if (btn_izq) {
+    btn_izq.addEventListener("click",()=> izquierda());
 }
 
-function Right() {
+function derecha() {
     if (cont >=carrousel.length-1) {
         cont=0;
         op=0;
@@ -30,7 +26,7 @@ function Right() {
     slider.style.transition="all ease .8s"
     }
 }
-function left() {
+function izquierda() {
     cont--;
     if (cont <0) {
         cont=carrousel.length-1;
@@ -46,68 +42,67 @@ function left() {
 
 function validar(){
     let
-    nombre=document.getElementById("nombres"),
-    apellido=document.getElementById("apellido"),
-    email=document.getElementById("correo"),
-    consulta=document.getElementById("consulta"),
-    errores=document.getElementById("result"),
-    Enviado=document.getElementById("result_enviado");
-     let errores_1;
-     nombre.classList.remove("error")
-     apellido.classList.remove("error")
-     consulta.classList.remove("error")
-     email.classList.remove("error")
-     Enviado.innerHTML="";
-     errores.innerHTML="";
-     if (nombre.value=="" ||nombre.value.length>20) {
-         errores_1=document.createElement("p");
-         errores.appendChild(errores_1);
-         errores_1.innerHTML="Error. Nombre incorrecto";
-         nombre.classList.add("error")
+        nombre=document.getElementById("nombres"),
+        apellido=document.getElementById("apellido"),
+        email=document.getElementById("correo"),
+        consulta=document.getElementById("consulta"),
+        error=document.getElementById("result"),
+        Enviado=document.getElementById("result_enviado");
+    let error2,datos;
+        nombre.classList.remove("error")
+        apellido.classList.remove("error")
+        consulta.classList.remove("error")
+        email.classList.remove("error")
+        Enviado.innerHTML="";
+        error.innerHTML="";
+    if (nombre.value=="" ||nombre.value.length>20) {
+        error2=document.createElement("p");
+        error.appendChild(error2);
+        error2.innerHTML="Error. Nombre incorrecto";
+        nombre.classList.add("error")
      }
-     if (apellido.value=="" ||nombre.value.length>20) {
-         errores_1=document.createElement("p");
-         errores.appendChild(errores_1);
-         errores_1.innerHTML="Error. Apellido incorrecto";
-         apellido.classList.add("error")
-     }
-     if (consulta.value=="" ||nombre.value.length>200) {
-         errores_1=document.createElement("p");
-         errores.appendChild(errores_1);
-         errores_1.innerHTML="Error. Consulta incorrecta";
-         consulta.classList.add("error")
-     }
-     if (email.value=="") {
-         errores_1=document.createElement("p");
-         errores.appendChild(errores_1);
-         errores_1.innerHTML="Error. Email incorrecto";
-         email.classList.add("error")
-     }
-     if (nombre.value.length>=1 &&
-         apellido.value.length>=1 &&
-         consulta.value.length>=1 &&
-         email.value.length>=1) {
-             errores_1=document.createElement("p");
-             errores.appendChild(errores_1);
-             errores_1.innerHTML="Consulta enviada correctamente";
-             contador3=true;
-     }else{
-         contador3=false;
-     }
-     let datos
-     if (contador3) {
-         datos=document.createElement("p");
-         Enviado.appendChild(datos);
-         datos.innerHTML="Su nombre: "+nombre.value;
-         datos=document.createElement("p");
-         Enviado.appendChild(datos);
-         datos.innerHTML="Su apellido: "+apellido.value;
-         datos=document.createElement("p");
-         Enviado.appendChild(datos);
-         datos.innerHTML="Su email: "+email.value;
-         datos=document.createElement("p");
-         Enviado.appendChild(datos);
-         datos.innerHTML="Su consulta: "+consulta.value; 
-     }
-     return false;
- }
+    if (apellido.value=="" ||nombre.value.length>20) {
+        error2=document.createElement("p");
+        error.appendChild(error2);
+        error2.innerHTML="Error. Apellido incorrecto";
+        apellido.classList.add("error")
+    }
+    if (consulta.value=="" ||nombre.value.length>200) {
+        error2=document.createElement("p");
+        error.appendChild(error2);
+        error2.innerHTML="Error. Consulta incorrecta";
+        consulta.classList.add("error")
+    }
+    if (email.value=="") {
+        error2=document.createElement("p");
+        error.appendChild(error2);
+        error2.innerHTML="Error. Email incorrecto";
+        email.classList.add("error")
+    }
+    if (nombre.value.length>=1 &&
+        apellido.value.length>=1 &&
+        consulta.value.length>=1 &&
+        email.value.length>=1) {
+        error2=document.createElement("p");
+        error.appendChild(error2);
+        error2.innerHTML="Consulta enviada correctamente";
+        contador3=true;
+    }else{
+        contador3=false;
+    }    
+    if (contador3) {
+        datos=document.createElement("p");
+        Enviado.appendChild(datos);
+        datos.innerHTML="Su nombre: "+nombre.value;
+        datos=document.createElement("p");
+        Enviado.appendChild(datos);
+        datos.innerHTML="Su apellido: "+apellido.value;
+        datos=document.createElement("p");
+        Enviado.appendChild(datos);
+        datos.innerHTML="Su email: "+email.value;
+        datos=document.createElement("p");
+        Enviado.appendChild(datos);
+        datos.innerHTML="Su consulta: "+consulta.value; 
+    }
+    return false;
+}
